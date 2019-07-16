65eeec079e30860cabb69429cdf75a0dd54daa07
Luke Nguyen <lukehaonguyen@gmail.com>
2019-07-16 16:08:00 -0600

    basic api set up

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, jsonify
 )
 from werkzeug.exceptions import abort
 
@@ -12,12 +12,26 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
+    cards = db.execute(
+        'SELECT c.id, title,  created'
+        ' FROM cards c'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+    tasks = db.execute(
+        'SELECT t.id, body, created, pcard'
+        ' FROM tasks t'
+        ' ORDER BY created ASC'
+    ).fetchall()
+    cdata = []
+    tdata = []
+    for c in cards:
+        cdata.append([x for x in c])
+    for t in tasks:
+        tdata.append([x for x in t])
+    # return render_template('blog/index.html', cards=cards, tasks=tasks)
+    print(cdata, tdata)
+    # return (cards, tasks)
+    return render_template('blog/index.html', cards=cards, tasks=tasks)
 
 
 @bp.route('/create', methods=('GET', 'POST'))
@@ -99,3 +113,53 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
+
+
+@bp.route('/addCard', methods=('GET', 'POST'))
+@login_required
+def addCard():
+    if request.method == 'POST':
+        title = request.form['title']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO cards (title)'
+                ' VALUES (?)',
+                (title,)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/addCard.html')
+
+
+@bp.route('/<int:id>/addTask', methods=('GET', 'POST'))
+@login_required
+def addTask(id):
+    if request.method == 'POST':
+        body = request.form['body']
+        error = None
+
+        if not body:
+            error = 'Body is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO tasks (pcard, body)'
+                ' VALUES (?, ?)',
+                (id, body)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/addTask.html')
